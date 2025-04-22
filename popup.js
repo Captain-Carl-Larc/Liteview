35488ba150f02726de7deb7d8a6a2138cc79600c
Captain-Carl-Larc <155651529+Captain-Carl-Larc@users.noreply.github.com>
2025-04-22 11:51:33 +0300

    code refactor
    Refactored the js code. created a function to handle the save functionality

--- popup.js
+++ popup.js
@@ -1,22 +1,24 @@
 
 document.addEventListener('DOMContentLoaded', function() {
+  //getting necessary DOM elements and storing in variables
   const saveButton = document.getElementById('saveButton');
   const linkInput = document.getElementById('linkInput');
   const messageDiv = document.getElementById('message');
+  const savedLinksContainer = document.getElementById('saved-links-container');
+  const savedLinksUl = document.getElementById('saved-links-ul');
 
-  saveButton.addEventListener('click', function() {
+  //function when save clicked
+  function saveLink(link) {
+    //1. Take the link from input field
     const link = linkInput.value.trim();
+    //2. do smth
     if (link) {
-      chrome.storage.local.get({ 'savedLinks': [] }, function(data) {
-        const savedLinks = data.savedLinks;
-        savedLinks.push(link);
-        chrome.storage.local.set({ 'savedLinks': savedLinks }, function() {
-          messageDiv.textContent = 'Link saved!';
-          linkInput.value = ''; // Clear the input field
-        });
-      });
+      messageDiv.textContent = `${link} saved!`;
     } else {
       messageDiv.textContent = 'Please enter a link.';
     }
-  });
+  }
+
+  //adding event listener to the save button
+  saveButton.addEventListener('click', saveLink );
 });
